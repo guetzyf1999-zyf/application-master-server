6c1a5e447ac4968f39bbd572e52d2cdc39b3715a
ZYF <1827035641@qq.com>
2022-12-09 11:41:55 +0800

    项目瘦身清理

--- src/main/java/com/application/template/entity/menu/Menu.java
+++ src/main/java/com/application/template/entity/menu/Menu.java
@@ -4,8 +4,6 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
-import java.util.List;
-
 @TableName("menu")
 public class Menu {
     @TableId(value = "id", type = IdType.AUTO)
