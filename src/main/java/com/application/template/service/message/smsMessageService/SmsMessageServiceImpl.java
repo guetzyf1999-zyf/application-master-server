8dd8a2bbf99760bb6850b156aacff1a98b9d42c7
ZYF <1827035641@qq.com>
2022-12-12 14:36:08 +0800

    SMS短信错误处理完善

--- src/main/java/com/application/template/service/message/smsMessageService/SmsMessageServiceImpl.java
+++ src/main/java/com/application/template/service/message/smsMessageService/SmsMessageServiceImpl.java
@@ -1,8 +1,11 @@
 package com.application.template.service.message.smsMessageService;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +24,8 @@ import com.application.template.util.HttpUtil;
 @Service("smsMessageServiceImpl")
 public class SmsMessageServiceImpl implements MessageService, InitializingBean {
 
+    private static final Logger logger = LoggerFactory.getLogger(SmsMessageServiceImpl.class);
+
     @Autowired
     private SmsMessageConfigProps smsMessageConfigProps;
 
@@ -33,11 +38,87 @@ public class SmsMessageServiceImpl implements MessageService, InitializingBean {
         params.put("Uid", smsMessageConfigProps.getUid());
         params.put("smsMob", phone);
         params.put("smsText", smsText + captcha);
-        HttpUtil.httpGetWithParams(ExternalServiceAddress.SMS_MESSAGE_HOST, params);
+        String response = HttpUtil.httpGetWithParams(ExternalServiceAddress.SMS_MESSAGE_HOST, params);
+        int responseCode = Integer.parseInt(response);
+        logger.error(SmsExceptionType.getSmsExceptionTypeByIndex(responseCode).getMessage());
+		AppAssert.judge(0 > responseCode, new AppException("短信服务出错"));
     }
 
     @Override
     public void afterPropertiesSet() {
         MessageSendingServiceFactory.putMessageService(MessageSendingApproach.SMS, this);
+    }
+
+    private enum SmsExceptionType {
+        /*
+         * 没有该用户账户
+         * */
+        NO_SUCH_USER("没有该用户账户", -1),
+        /*
+         * 接口密钥不正确,不是账户登陆密码
+         * */
+        INCORRECT_KEY("接口密钥不正确,不是账户登陆密码", -2),
+        /*
+         * MD5接口密钥加密不正确
+         * */
+        INCORRECT_MD5_KEY("MD5接口密钥加密不正确", -21),
+        /*
+         * 短信数量不足
+         * */
+        SMS_OUT_OF_NUMBER("短信数量不足", -3),
+        /*
+         * 该用户被禁用
+         * */
+        USER_DISABLE("该用户被禁用", -11),
+        /*
+         * 短信内容出现非法字符
+         * */
+        SMS_TEXT_ILLEGAL("短信内容出现非法字符", -14),
+        /*
+         * 手机号格式不正确
+         * */
+        PHONE_FORMAT_WRONG("手机号格式不正确", -4),
+        /*
+         * 手机号码为空
+         * */
+        EMPTY_PHONE("手机号码为空", -41),
+        /*
+         * 短信内容为空
+         * */
+        EMPTY_SMS_TEXT("短信内容为空", -42),
+        /*
+         * 短信签名格式不正确
+         * */
+        SMS_SIGNATURE_FORMAT_WRONG("短信签名格式不正确", -51),
+        /*
+         * 短信签名太长建议签名10个字符以内
+         * */
+        SMS_SIGNATURE_EXCEEDING_LENGTH("短信签名太长建议签名10个字符以内", -52),
+        /*
+         * IP限制
+         * */
+        IP_RESTRICTED("IP限制", -6);
+
+        private final String message;
+        private final Integer index;
+
+        SmsExceptionType(String message, Integer index) {
+            this.message = message;
+            this.index = index;
+        }
+
+        private static SmsExceptionType getSmsExceptionTypeByIndex(Integer index) {
+			return Arrays.stream(SmsExceptionType.values())
+					.filter(type -> index.equals(type.index)).findFirst()
+					.orElseThrow(() -> new AppException("未找到对应短信错误类型"));
+		}
+
+        private String getMessage() {
+            return message;
+        }
+
+        private Integer getIndex() {
+            return index;
+        }
     }
 }
